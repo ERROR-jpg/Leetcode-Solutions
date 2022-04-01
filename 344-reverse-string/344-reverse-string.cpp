46f00e9eb09e3e9fd5faf567995b2681486a0696
ERROR-jpg <78738408+ERROR-jpg@users.noreply.github.com>
2022-04-01 12:58:42 +0530

    Time: 11 ms (99.71%), Space: 23 MB (93.80%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    void reverseString(vector<char>& str) {
+        int n = str.size();
+        for (int i = 0; i < n / 2; i++)
+        swap(str[i], str[n - i - 1]);
+    }
+};
