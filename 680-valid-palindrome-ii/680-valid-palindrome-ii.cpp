9f7f899713cb3ebddaf07791c732baf857d26c20
ERROR-jpg <78738408+ERROR-jpg@users.noreply.github.com>
2022-04-02 11:03:19 +0530

    Time: 50 ms (96.88%), Space: 23.7 MB (49.20%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool checkpalindrome(string s, int i , int j){
+        while(i<j){
+            if(s[i]!=s[j]){
+                return false;
+            }
+            j--;
+            i++;
+        }
+        return true;
+    }
+    bool validPalindrome(string s) {
+        int i =0;
+        int j = s.size()-1;
+        while(i<j){
+            if(s[i]!=s[j])
+            { 
+                return checkpalindrome(s,i,j-1) || checkpalindrome(s, i+1,j);
+            }
+            j--;
+            i++;
+        }
+        return true;
+        
+    }
+};
