80af3317ac535d3b25e3446c58a604d791126969
ERROR-jpg <78738408+ERROR-jpg@users.noreply.github.com>
2022-04-21 11:24:44 +0530

    Time: 6 ms (28.50%), Space: 6.1 MB (74.31%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    int countGoodSubstrings(string s) {
-        int count = 0;
-        if(s.size()<3) return 0;
-        for(int i = 0; i<s.length()-2;i++){
-            set<char> st;
-            {
-                for (int j = i; j<i+3;j++){
-                    char ch = s[j];
-                    st.emplace(ch);
-                }
-                if(st.size()==3){
-                    count++;
-                }
-            }
-        }
-        return count;
-    }
+int countGoodSubstrings(string s) {
+int i=0,j=0,count=0;
+while(j<s.size()){
+if(j-i+1<3)
+j++;
+else if(j-i+1==3){
+if(s[i]!=s[i+1] && s[i+1]!=s[i+2] && s[i]!=s[i+2])
+count++;
+i++;
+j++;
+}
+}
+return count;
+}
 };
