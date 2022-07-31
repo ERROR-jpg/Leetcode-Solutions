08df7cc6c8003236d7462395451ef84b5478ebcd
ERROR-jpg <78738408+ERROR-jpg@users.noreply.github.com>
2022-07-31 18:07:17 +0530

    Time: 0 ms (100.00%), Space: 6 MB (22.81%) - LeetHub

--- 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.cpp
+++ 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.cpp
@@ -14,14 +14,22 @@ public:
         int start = 1, end = n;
         
         while(start<=end){
-            int mid = start+(end-start)/2;
-            int k = (guess(mid));
-            if(k==-1){
-                end = mid-1;
-            }else if(k==1){
-                start = mid+1;
-            }else{
-                return mid;
+            int mid1 = start+(end-start)/3;
+            int mid2 = end-(end-start)/3;
+            int res1 = guess(mid1);
+            int res2 = guess(mid2);
+            
+            if(res1==0)
+                return mid1;
+            if(res2==0)
+                return mid2;
+            else if(res1<0)
+                end = mid1-1;
+            else if(res2>0)
+                start=mid2+1;
+            else{
+                start = mid1+1;
+                end = mid2-1;
             }
         }
         return -1;
