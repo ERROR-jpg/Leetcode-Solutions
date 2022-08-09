414c46ec836c8ab8617d6e152d2f637d885ededd
ERROR-jpg <78738408+ERROR-jpg@users.noreply.github.com>
2022-08-09 09:45:08 +0530

    Time: 152 ms (33.39%), Space: 59.6 MB (31.38%) - LeetHub

---
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        if(arr.size()==1) return 0;
+        int low = 0;
+        int high = arr.size()-1;
+        int size = arr.size();
+        
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(mid>0 && mid<arr.size()-1){
+                if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1])
+                    return mid;
+                else if(arr[mid-1]>arr[mid]){
+                    high = mid-1;
+                }
+                else{
+                    low = mid+1;
+                }
+            }
+            else if(mid==0){
+                if(arr[0]>arr[1])
+                    return 0;
+               else{
+                return 1;
+            }
+            }
+            else if(mid==arr.size()-1)
+                if(arr[size-1]>arr[size-2])
+                    return size-1;
+                else
+                    return size-2;    
+        }
+        return -1;
+    }
+};
