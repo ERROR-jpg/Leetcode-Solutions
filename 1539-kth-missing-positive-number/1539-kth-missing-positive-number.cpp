9382e988f3724c1c6bb02c07b30ee73b196adafe
ERROR-jpg <78738408+ERROR-jpg@users.noreply.github.com>
2022-08-14 15:59:59 +0530

    Time: 7 ms (66.01%), Space: 9.6 MB (28.14%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -2,24 +2,9 @@ class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
         
-        set<int>s ;
-        
-        int n = arr.size();
-        int m = arr[n-1];
-    
-        for(int i =0; i<n; i++){
-            s.insert(arr[i]);
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]<=k)k++;
         }
-        
-        vector<int> v;
-        
-        
-        for(int i = 1; i<m+k+1; i++){
-            if(s.find(i)==s.end())
-                v.push_back(i);
-        }
-        
-        
-        return v[k-1];
+        return k;
     }
 };
