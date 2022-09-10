9f926c5a03cf80190085218d702799a68e37743d
ERROR-jpg <78738408+ERROR-jpg@users.noreply.github.com>
2022-09-10 13:01:36 +0530

    Time: 13 ms (71.79%), Space: 10.6 MB (98.77%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
-    int maxProfit(int k, vector<int>& arr) {
-        int n = arr.size();
+    int maxProfit(int k, vector<int>& prices) {
+        if (k == 0) return 0;
         
-        if (n<=1)
-            return 0;
-        
-        if(k>arr.size())k=arr.size();
-        
-        vector<vector<int>> dp(k+1, vector<int>(n,0));
-        
-        for(int i=1; i<k+1;i++){
-            int var = -arr[0]+dp[i-1][0];
-            for(int j =1; j<n;j++){
-                 for(int prevj=0; prevj<j;prevj++){
-                     var = max(var, -arr[prevj]+dp[i-1][prevj]);
-                 }
-               dp[i][j] = max(arr[j]+var, dp[i][j-1]);
+        vector<int> profit(k+1, 0);
+        vector<int> cost(k+1, INT_MAX);
+                
+        for (int price: prices) {
+            for (int i = 0; i < k; i++) {
+                cost[i+1] = min(cost[i+1], price - profit[i]);
+                profit[i+1] = max(profit[i+1], price - cost[i+1]);
             }
         }
-        return dp[k][n-1];
+        return profit[k];
     }
 };
