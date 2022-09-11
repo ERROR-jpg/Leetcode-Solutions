da004426f3097c9a41da11ba06d62f5aa34dabcc
ERROR-jpg <78738408+ERROR-jpg@users.noreply.github.com>
2022-09-11 10:19:39 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (70.17%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+    void sortColors(vector<int>& a) {
+       int count0 = 0;
+        int count1 = 0;
+        int count2 =0;
+        for(int i =0; i<a.size();i++){
+            if(a[i]==0){
+                count0++;
+            }else if(a[i]==1){
+            count1++;
+            }else
+            count2++;
+    } 
+   
+    int i =0;
+    while(count0--)a[i++]=0;
+    while(count1--)a[i++]=1;
+    while(count2--)a[i++]=2;
     }
 };
