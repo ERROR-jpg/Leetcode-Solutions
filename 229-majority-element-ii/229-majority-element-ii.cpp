7569308a29ddb9e8608ce1b014d3e52b557029cb
ERROR-jpg <78738408+ERROR-jpg@users.noreply.github.com>
2022-09-18 11:30:06 +0530

    Time: 28 ms (30.53%), Space: 15.8 MB (84.32%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-    vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int, int> mp;
-        vector<int> ans;
-         int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            mp[nums[i]]++;
-        }
-
-        int k = n/3;
-        
-        for(auto i:mp){
-            int check = i.second;
-            if(check>k)
-                ans.push_back(i.first);
-        }
-        
-        sort(ans.begin(), ans.end());
-        
-        return ans;
-    }
+  vector<int> majorityElement(vector<int> &a) 
+  {
+	  int y(-1), z(-1), cy(0), cz(0);
+    
+	  for (const auto & x: a) 
+	  {
+		  if (x == y) cy++;
+		  else if (x == z) cz++;
+		  else if (! cy) y = x, cy = 1;
+		  else if (! cz) z = x, cz = 1;
+		  else cy--, cz--;
+     }
+      
+     cy = cz = 0;
+     for (const auto & x: a)
+		 if (x == y) cy++;
+			 else if (x == z) cz++;
+  
+	  vector<int> r;
+	  if (cy > size(a)/3) r.push_back(y);
+	  if (cz > size(a)/3) r.push_back(z);
+	  return r;
+  }
 };
