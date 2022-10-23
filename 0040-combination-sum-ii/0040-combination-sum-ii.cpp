44d0717309e4240c3cb182b6454496db669adbc8
Jaymin Rajgor <78738408+ERROR-jpg@users.noreply.github.com>
2022-10-23 12:50:22 +0530

    Time: 7 ms (80.24%), Space: 10.7 MB (73.53%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    
+    void findCombinations(int ind, vector<vector<int>>&ans,vector<int>&arr, vector<int>&ds, int target){
+        if(target==0){
+            ans.push_back(ds);
+            return;
+        }
+        
+        for(int i =ind; i<arr.size(); i++){
+            if(i>ind&&arr[i]==arr[i-1]) continue;
+            if(arr[i]>target)break;
+            ds.push_back(arr[i]);
+            findCombinations(i+1, ans, arr,ds, target-arr[i]);
+            ds.pop_back();
+        }
+    }
+    
+    vector<vector<int>> combinationSum2(vector<int>& arr, int target) {
+        sort(arr.begin(), arr.end());
+        
+        vector<vector<int>> ans;
+        vector<int> ds;
+        findCombinations(0, ans, arr, ds, target);
+        return ans;
+    }
+};
