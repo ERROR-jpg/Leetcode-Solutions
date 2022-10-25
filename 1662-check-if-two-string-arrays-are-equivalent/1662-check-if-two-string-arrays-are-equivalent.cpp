9ecfad5dae168c79b3e55cf05bec373e5aa70df2
Jaymin Rajgor <78738408+ERROR-jpg@users.noreply.github.com>
2022-10-25 13:08:14 +0530

    Time: 6 ms (74.31%), Space: 11.1 MB (99.96%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
-         int n = word1.size(), m = word2.size();
-        // pointer to the words in word1 and word2
-        int i = 0, j = 0;
-        // pointer to the character in `word1[i]` and `word2[j]`
-        int x = 0, y = 0;
-        while (i < n && j < m) {
-            // if both character doesn't match, then return false
-            // remember to increase the pointer x and y 
-            // otherwise, it will cause TLE
-            if (word1[i][x++] != word2[j][y++]) return false;
-            // reach the end of the current word
-            // move to another word
-            // reset the pointer to 0 for the new word
-            if (x == word1[i].size()) i += 1, x = 0;
-            if (y == word2[j].size()) j += 1, y = 0;
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) 
+    {
+        int wordIdx1 = 0, wordIdx2 = 0, chIdx1 = 0, chIdx2 = 0;
+        while(true)
+        {
+            char ch1 = word1[wordIdx1][chIdx1];
+            char ch2 = word2[wordIdx2][chIdx2];
+            if (ch1 != ch2) return false;
+            
+            chIdx1++;
+            chIdx2++;
+            if (chIdx1 == word1[wordIdx1].size()) { wordIdx1++; chIdx1 = 0; }
+            if (chIdx2 == word2[wordIdx2].size()) { wordIdx2++; chIdx2 = 0; }
+            
+            if (wordIdx1 == word1.size() && wordIdx2 == word2.size()) break;
+            if (wordIdx1 == word1.size() || wordIdx2 == word2.size()) return false;
         }
-        // if they are equivalent, word pointers would be at position n and m 
-        return i == n && j == m;
+        return true; 
     }
-    
 };
