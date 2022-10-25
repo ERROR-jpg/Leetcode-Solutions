6175456298f42a8faf9e5da360af46759475bae3
Jaymin Rajgor <78738408+ERROR-jpg@users.noreply.github.com>
2022-10-25 13:07:50 +0530

    Time: 21 ms (5.57%), Space: 11.3 MB (89.94%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
-        string res1="";
-        string res2="";
-        
-        for(int i=0; i<word1.size(); i++){
-            res1+=word1[i];
+         int n = word1.size(), m = word2.size();
+        // pointer to the words in word1 and word2
+        int i = 0, j = 0;
+        // pointer to the character in `word1[i]` and `word2[j]`
+        int x = 0, y = 0;
+        while (i < n && j < m) {
+            // if both character doesn't match, then return false
+            // remember to increase the pointer x and y 
+            // otherwise, it will cause TLE
+            if (word1[i][x++] != word2[j][y++]) return false;
+            // reach the end of the current word
+            // move to another word
+            // reset the pointer to 0 for the new word
+            if (x == word1[i].size()) i += 1, x = 0;
+            if (y == word2[j].size()) j += 1, y = 0;
         }
-        for(int i=0; i<word2.size(); i++){
-            res2+=word2[i];
-        }
-        if(res1==res2) return true;
-        
-        return false;
+        // if they are equivalent, word pointers would be at position n and m 
+        return i == n && j == m;
     }
     
 };
