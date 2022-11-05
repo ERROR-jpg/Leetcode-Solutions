951dc854fef8f656bdaa690cc4110df23b03cc07
Jaymin Rajgor <78738408+ERROR-jpg@users.noreply.github.com>
2022-11-05 20:05:36 +0530

    Time: 30 ms (99.84%), Space: 29.4 MB (5.21%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.cpp
+++ 0212-word-search-ii/0212-word-search-ii.cpp
@@ -1,74 +1,228 @@
-class Trie {
-    public:
-        unordered_map<char, Trie*> child;
-        bool isEnd = false;
-        string word;
-};
-class Solution {
-public:
-    Trie* root = new Trie();
-
-    void insertWord(string &s)
+class Solution
+{
+    struct TrieNode
     {
-        Trie* temp = root;
-        for(char c : s)
-        {
-            if(temp->child.find(c) == temp->child.end())
-                temp->child[c] = new Trie();
-            temp = temp->child[c];
-        }
-        temp->isEnd = true;
-        temp->word = s;
+        uint32_t    References{};
+        bool        IsWord{};
+        std::array<std::unique_ptr<TrieNode>, 26>   Children{};
+    };
+
+private:
+
+    inline size_t charToIndex(char ch) const
+    {
+        return static_cast<size_t>(ch - 'a');
     }
-    vector<string> ans;
-    bool isValid(int i, int j, int n, int m)
+
+    void reverseWords(std::vector<std::string> &words)
     {
-        return (i >= 0 and j >= 0 and i < n and j < m);
-    }
-    void rec(int i, int j, int n, int m, vector<vector<char>> &board, vector<vector<bool>> &vis, Trie* temp)
-    {
-        if(temp->isEnd)
+        for (auto& word : words)
         {
-            temp->isEnd = false;
-            ans.push_back(temp->word);
+            std::reverse(std::begin(word), std::end(word));
         }
-        int dir[5] = {-1, 0, 1, 0, -1};
+    }
 
-        for(int k=0; k<4; ++k)
+    bool removeWord(TrieNode* root, std::string const& word) const
+    {
+        if (!root)
         {
-            int new_i = i + dir[k];
-            int new_j = j + dir[k+1];
+            assert(root);
+            return false;
+        }
 
-            if(isValid(new_i, new_j, n, m) && temp->child.find(board[new_i][new_j]) != temp->child.end() && !vis[new_i][new_j])
+        TrieNode* node = root;
+
+        for (char ch : word)
+        {
+            node = node->Children[charToIndex(ch)].get();
+
+            if (!node)
             {
-                vis[new_i][new_j] = true;
+                assert(node);
+                return false;
+            }
 
-                rec(new_i, new_j, n, m, board, vis, temp->child[board[new_i][new_j]]);
+            node->References--;
+            assert(node->References >= 0);
+        }
 
-                vis[new_i][new_j] = false;
+        if (node->IsWord)
+        {
+            node->IsWord = false;
+            return true;
+        }
+
+        return false;
+    }
+
+    std::unique_ptr<TrieNode> buildTrie(std::vector<std::string> const& words) const
+    {
+        auto root = std::make_unique<TrieNode>();
+
+        // Note: The word list is supposed to be unique. 
+        std::set<std::string> const uniqueWords{ std::begin(words), std::end(words) };
+
+        for (std::string const& word : uniqueWords)
+        {
+            TrieNode* node = root.get();
+
+            for (char ch : word)
+            {
+                TrieNode* nextNode = node->Children[charToIndex(ch)].get();
+
+                if (!nextNode)
+                {
+                    nextNode = new TrieNode;
+                    node->Children[charToIndex(ch)].reset(nextNode);
+                }
+
+                node = nextNode;
+                node->References++;
+            }
+
+            if (node)
+            {
+                node->IsWord = true;
             }
         }
+
+        return root;
     }
-    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
-        for(auto s : words)
-            insertWord(s);
 
-        int n = board.size(), m = board[0].size();
-        vector<vector<bool>> vis(n, vector<bool>(m));
-
-        for(int i=0; i<n; ++i)
+    void findWords(std::vector<std::vector<char>>& board, int row, int col, TrieNode* root, TrieNode* node, std::string& prefix, std::vector<string>& foundWords)
+    {
+        if (row < 0 || row >= board.size() || col < 0 || col >= board[0].size())
         {
-            for(int j=0; j<m; ++j)
+            return;
+        }
+
+        constexpr char VISITED_CELL = '#';
+
+        char& cell = board[row][col];
+
+        if (cell == VISITED_CELL)
+        {
+            return;
+        }
+
+        char const ch = cell;
+
+        node = node->Children[charToIndex(ch)].get();
+
+        if (!node || node->References <= 0)
+        {
+            return;
+        }
+
+        prefix.push_back(ch);
+
+        if (node->IsWord)
+        {
+            removeWord(root, prefix);
+            foundWords.emplace_back(prefix);
+        }
+
+        cell = VISITED_CELL;
+
+        findWords(board, row + 1, col, root, node, prefix, foundWords);
+        findWords(board, row - 1, col, root, node, prefix, foundWords);
+        findWords(board, row, col + 1, root, node, prefix, foundWords);
+        findWords(board, row, col - 1, root, node, prefix, foundWords);
+
+        cell = ch;
+
+        prefix.pop_back();
+    }
+    
+    struct StartingLocations
+    {
+        std::vector<std::pair<int, int>>    Locations{};
+        bool Reverse{};
+    };
+    
+    StartingLocations getStartingLocations(std::vector<std::vector<char>> const& board, std::vector<std::string> const& words) const
+    {
+        int const numRows = static_cast<int>(board.size());
+        int const numCols = static_cast<int>(board[0].size());
+
+        std::unordered_map<char, size_t> charCount, charCountReverse;
+        {
+            for (auto const& word : words)
             {
-                if(root->child.find(board[i][j]) != root->child.end())
+                if (!word.empty())
                 {
-                    Trie* temp = root;
-                    vis[i][j] = true;
-                    rec(i, j, n, m, board, vis, temp->child[board[i][j]]);
-                    vis[i][j] = false;
+                    charCount[word.front()]++;
+                    charCountReverse[word.back()]++;
                 }
             }
         }
-        return ans;
+
+        size_t total = 0, totalReverse = 0;
+        std::vector<std::pair<int, int>> startingLocations, startingLocationsReverse;
+        {
+            for (int row = 0; row < numRows; ++row)
+            {
+                for (int col = 0; col < numCols; ++col)
+                {
+                    char const ch = board[row][col];
+
+                    auto it = charCount.find(ch);
+                    if (it != std::end(charCount))
+                    {
+                        startingLocations.emplace_back(row, col);
+                        total = startingLocations.size() * it->second;
+                    }
+
+                    auto it2 = charCountReverse.find(ch);
+                    if (it2 != std::end(charCountReverse))
+                    {
+                        startingLocationsReverse.emplace_back(row, col);
+                        totalReverse = startingLocationsReverse.size() * it2->second;
+                    }
+                }
+            }
+        }
+
+        if (totalReverse < total)
+        {
+            return StartingLocations{ startingLocationsReverse, true };
+        }
+        
+        return StartingLocations{ startingLocations, false };
+    }
+
+public:
+
+    Solution()
+    {
+
+    }
+
+    std::vector<std::string> findWords(std::vector<std::vector<char>>& board, std::vector<std::string>& words)
+    {
+        StartingLocations locations = getStartingLocations(board, words);
+
+        if (locations.Reverse)
+        {
+            reverseWords(words);
+        }
+        
+        auto trieRoot = buildTrie(words);
+
+        words.clear();
+        words.shrink_to_fit();
+
+        for (auto const& [row, col] : locations.Locations)
+        {
+            std::string prefix;
+            findWords(board, row, col, trieRoot.get(), trieRoot.get(), prefix, words);
+        }
+
+        if (locations.Reverse)
+        {
+            reverseWords(words);
+        }
+
+        return words;
     }
 };
