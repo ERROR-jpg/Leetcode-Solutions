63dfcf5851e6a92d859f1882f28bbc2ff4ed1710
Jaymin Rajgor <78738408+ERROR-jpg@users.noreply.github.com>
2023-01-18 12:59:06 +0530

    Time: 5 ms (42.43%), Space: 7 MB (96.14%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    void findsubsets(int ind, vector<int>&nums, vector<vector<int>>&ans, vector<int>&ds){
-        ans.push_back(ds);
-        
-        for(int i=ind; i<nums.size();i++){
-            if(i!=ind&& nums[i]==nums[i-1])continue;
-            ds.push_back(nums[i]);
-            findsubsets(i+1, nums, ans, ds);
-            ds.pop_back();
-        }
-    }
-    
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> ds;
-        findsubsets(0, nums, ans, ds);
-        return ans;
+        vector<vector<int>> subs;
+        vector<int> sub;
+        subsets(nums, 0, sub, subs);
+        return subs;
+    }
+private:
+    void subsets(vector<int>& nums, int i, vector<int>& sub, vector<vector<int>>& subs) {
+        subs.push_back(sub);
+        for (int j = i; j < nums.size(); j++) {
+            sub.push_back(nums[j]);
+            subsets(nums, j + 1, sub, subs);
+            sub.pop_back();
+        }
     }
 };
