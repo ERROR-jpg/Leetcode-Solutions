111afb5bfc8213675af6b62bd21b5026919fca57
Jaymin Rajgor <78738408+ERROR-jpg@users.noreply.github.com>
2023-02-02 14:02:44 +0530

    Time: 6 ms (21.15%), Space: 8.5 MB (19.36%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -11,20 +11,23 @@
  */
 class Solution {
 public:
-    vector<int> answer;
-    
-    void dfs(TreeNode* node){
-        if(node == nullptr){
-            return;
-        }
-        
-        answer.push_back(node->val);
-        dfs(node->left);
-        dfs(node->right);
-    }
-    
     vector<int> preorderTraversal(TreeNode* root) {
-       dfs(root);
-           return answer;
+        vector<int> preorder;
+        if(root==NULL) return preorder;
+        
+        stack<TreeNode*> st;
+        st.push(root);
+        while(!st.empty()){
+            root = st.top();
+            st.pop();
+            preorder.push_back(root->val);
+            if(root->right != NULL){
+                st.push(root->right);
+            }
+            if(root->left != NULL){
+                st.push(root->left);
+            }
+        }
+        return preorder;
     }
 };
