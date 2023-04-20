fda6631a9b5bd3a6dbd9e01ef73b4170b5113bec
Jaymin Rajgor <78738408+ERROR-jpg@users.noreply.github.com>
2023-04-20 07:25:35 +0530

    Time: 11 ms (52.46%), Space: 17.3 MB (91.98%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -12,32 +12,36 @@
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        if(!root)
+        if (root == nullptr) {
             return 0;
+        }
         
-        int ans = 0;
+        int max_width = 1;
         queue<pair<TreeNode*, int>> q;
         q.push({root, 0});
         
-        while(!q.empty()){
-            int size = q.size();
-            int mmin = q.front().second;
-            int first, last;
+        while (!q.empty()) {
+            int level_size = q.size();
+            int start_index = q.front().second;
+            int end_index = q.back().second;
+            max_width = max(max_width, end_index - start_index + 1);
             
-            for(int i =0;  i<size;i++){
-                int cur_id = q.front().second-mmin;
-                TreeNode* node = q.front().first;
+            for (int i = 0; i < level_size; ++i) {
+                auto node_index_pair = q.front();
+                TreeNode* node = node_index_pair.first;
+                int node_index = node_index_pair.second - start_index;
                 q.pop();
-                if(i==0) first = cur_id;
-                if(i==size-1) last = cur_id;
-                if(node->left)
-                    q.push({node->left, (long long)cur_id*2+1});
-                if(node->right)
-                    q.push({node->right, (long long) cur_id*2+2});
-
+                
+                if (node->left != nullptr) {
+                    q.push({node->left, 2LL * node_index + 1});
+                }
+                
+                if (node->right != nullptr) {
+                    q.push({node->right, 2LL * node_index + 2});
+                }
             }
-            ans = max(ans, last-first+1);
         }
-        return ans;
+        
+        return max_width;
     }
 };
