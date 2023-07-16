be783cbcd0e748b910e84d276eb53fa48af0b675
Jaymin Rajgor <78738408+ERROR-jpg@users.noreply.github.com>
2023-07-16 14:58:46 +0530

    Time: 6 ms (51.70%), Space: 7.3 MB (91.29%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int start=0;
-       int end=s.size()-1;
-       while(start<=end){
-           if(!isalnum(s[start])){start++; continue;}
-           if(!isalnum(s[end])){end--;continue;}
-           if(tolower(s[start])!=tolower(s[end]))return false;
-           else{
-               start++;
-               end--;
-           }
-       }
-       return true;
+        int i =0;
+        int j = s.size()-1;
+        
+        while(i<j){
+            while(!isalnum(s[i]) && i<j){
+                i++;
+            }
+            while(!isalnum(s[j])&& i<j){
+                j--;
+            }
+            if(tolower(s[i])!=tolower(s[j])){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
         
     }
 };
