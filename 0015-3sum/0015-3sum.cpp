f7baf2e647e7d234a799f1f15d720ffca0ed7961
Jaymin Rajgor <78738408+ERROR-jpg@users.noreply.github.com>
2023-07-18 21:14:36 +0530

    Time: 1516 ms (19.96%), Space: 191.5 MB (20.58%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& num) {
-        sort(num.begin(), num.end());
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int target = 0;
+        sort(nums.begin(), nums.end());
+        set<vector<int>> s;
+        vector<vector<int>> output;
         
-        vector<vector<int>> res;
-        
-        for(int i=0; i<num.size();i++){
-            if(i==0 || (i>0 && num[i]!=num[i-1])){
-                int lo = i+1, hi = num.size()-1, sum=0-num[i];
-                
-                while(lo < hi){
-                    if(num[lo] + num[hi] == sum){
-                        vector<int> temp;
-                        temp.push_back(num[i]);
-                        temp.push_back(num[lo]);
-                        temp.push_back(num[hi]);
-                        res.push_back(temp);
-                        
-                        while(lo<hi && num[lo] == num[lo+1]) lo++;
-                        while(lo<hi && num[hi] == num[hi-1]) hi--;
-                        
-                        lo++; hi--;
-                    }
-                    else if (num[lo] + num[hi] < sum) lo++;
-                    else hi--;
+        for(int i = 0; i<nums.size(); i++){
+            int j = i+1;
+            int k = nums.size() - 1;
+            while(j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum == target){
+                    s.insert({nums[i], nums[j], nums[k]});
+                    j++;;
+                    k--;
+                }else if(sum<target){
+                    j++;
+                }else{
+                    k--;
                 }
             }
         }
-        return res;
+        for(auto triplets : s)
+            output.push_back(triplets);
+        
+        return output;
+        
     }
 };
