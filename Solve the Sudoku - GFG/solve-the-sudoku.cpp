a514cc6ac3d4755fdc7cad145b175f70ee9bd15f
Jaymin Rajgor <78738408+ERROR-jpg@users.noreply.github.com>
2023-08-07 23:43:56 +0530

    Added solution - LeetHub

--- Solve the Sudoku - GFG/solve-the-sudoku.cpp
+++ Solve the Sudoku - GFG/solve-the-sudoku.cpp
@@ -9,40 +9,32 @@ using namespace std;
 #define N 9  
 
 
-// } Driver Code Endsclass Solution 
-
+// } Driver Code Ends
 
 class Solution 
 {
     public:
-    bool issafe(int grid[N][N],int row,int col,int val){
-        for(int i=0;i<9;i++){
-            if(grid[row][i]==val)
-                return false;
-            if(grid[i][col]==val)
-                return false;
-            if(grid[3*(row/3)+i/3][3*(col/3)+i%3]==val)
-                return false;
-        }
-        return true;
-    }
     //Function to find a solved Sudoku. 
     bool SolveSudoku(int grid[N][N])  
     { 
-        for(int row=0;row<9;row++){
-            for(int col=0;col<9;col++){
-                if(grid[row][col]==0){
-                    for(int val=1;val<=9;val++){
-                        if(issafe(grid,row,col,val)){
-                            grid[row][col]=val;
-                        bool furtherpossible= SolveSudoku(grid);
-                        if(furtherpossible)
-                            return true;
-                        else
-                            grid[row][col]=0;
+        solve(grid);
+    }
+    bool solve(int grid[N][N]){
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                if(grid[i][j]==0){
+                    
+                    for(int d=1;d<=9;d++){
+                        if(isValid(grid, i, j, d)){
+                            grid[i][j]=d;
+                            
+                            if(solve(grid)==true){
+                                return true;
+                            }
+                            else{
+                                grid[i][j]=0;
+                            }
                         }
-                        //then check for further solution 
-                        
                     }
                     return false;
                 }
@@ -51,17 +43,30 @@ class Solution
         return true;
     }
     
+    bool isValid(int grid[N][N], int row, int col, int d){
+        for(int i=0;i<9;i++){
+            if(grid[row][i]==d) return false;
+            if(grid[i][col]==d) return false;
+            
+            if(grid[ 3*(row/3)+ i/3 ][ 3*(col/3) +i%3 ]==d ) return false;
+        }
+        
+        return true;
+    }
+    
     //Function to print grids of the Sudoku.
     void printGrid (int grid[N][N]) 
     {
-       
-       for(int i=0;i<N;i++){
-           for(int j=0;j<N;j++){
-               cout<<grid[i][j]<<" ";
-           }
-       }
+        if(SolveSudoku(grid)){
+            for(int i=0;i<N;i++){
+                for(int j=0;j<N;j++){
+                    cout<<grid[i][j]<<" ";
+                }
+            }
+        }
     }
 };
+
 
 //{ Driver Code Starts.
 
