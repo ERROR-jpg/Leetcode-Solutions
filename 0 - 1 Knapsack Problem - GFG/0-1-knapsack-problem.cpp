91e90062e93af68323c0996339c9ca790527c2b5
Jaymin Rajgor <78738408+ERROR-jpg@users.noreply.github.com>
2023-08-27 16:30:42 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -9,27 +9,24 @@ class Solution
     public:
     //Function to return max value that can be put in knapsack of capacity W.
     int knapSack(int w, int wt[], int val[], int n) 
-    {
-        int t[n+1][w+1];
-        
-        //Initialize matrix
-        for(int i=0; i<w+1; i++) t[0][i] = 0;
-        for(int j=0; j<n+1; j++) t[j][0] = 0;
-        
-        //Choice Diagram to code
-        for(int i = 1; i<n+1; i++){
-            for(int j=1; j<w+1; j++){
-                //1. In case of choice
-                if(wt[i-1] <= j){
-                    t[i][j] = max(val[i-1] + t[i-1][j-wt[i-1]], 0+t[i-1][j]);
-                }
-                //2. No choice
-                else if(wt[i-1] > j){
-                    t[i][j] = 0+t[i-1][j];
-                }
-            }
-        }
-        return t[n][w];
+    { 
+       // Your code here
+       int dp[n+1][w+1];
+       for(int i=0; i<w+1; i++) dp[0][i] =0;
+       for(int j=0; j<n+1; j++) dp[j][0] = 0;
+       
+       
+       for(int i=1; i<n+1; i++){
+           for(int j =1; j<w+1; j++){
+               if(wt[i-1]<=j){
+                   dp[i][j] = max(val[i-1]+dp[i-1][j-wt[i-1]], 0+dp[i-1][j]);
+               } else if(wt[i-1] > j){
+                   dp[i][j] = 0 + dp[i-1][j];
+               }
+           }
+       }
+      
+       return dp[n][w];
     }
 };
 
